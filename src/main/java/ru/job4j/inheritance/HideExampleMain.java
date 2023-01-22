5335112c95157921200cc25e3aa54535e3655b6d
elior <DarknerY_037131>
2023-01-22 17:38:58 +0300

    Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -0,0 +1,21 @@
+package ru.job4j.inheritance;
+
+public class HideExampleMain {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat = new Cat();
+        animal.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+
+        Animal animal1 = new Animal();
+        animal1.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+    }
+
+
+}

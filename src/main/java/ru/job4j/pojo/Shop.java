7b75d6328f375640f6b49a6301bfa38d40598ab4
elior <DarknerY_037131>
2023-01-31 00:50:39 +0300

    Массив с пустыми ячейками [#176470]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -5,7 +5,6 @@ public class Shop {
     public static int indexOfNull(Product[] products) {
         int a = -1;
         for (int index = 0; index < products.length; index++) {
-           // Product product = products[index];
             if (products[index] == null) {
                  a = index;
                  break;

ecc5118a780f12c591b53d52353a150fed113c70
elior <DarknerY_037131>
2023-02-08 00:49:50 +0300

    Tracker - хранилище [#396]

---
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Item[] findAll() {
+        for (int i = 0; i < size; i++) {
+            if (items[i] == null) {
+                for (int index = i; i < size - 1; i++) {
+                    items[index] = items[index + 1];
+                }
+            }
+        }
+        return items;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(items[i].getName())) {
+                rsl[index] = item;
+                index += 1;
+            }
+        }
+            return Arrays.copyOf(rsl, index);
+        }
+}

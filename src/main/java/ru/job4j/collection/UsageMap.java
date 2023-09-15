5d7b4168bc3bf74928528596617c9f48e9a2caa5
DarknerY98 <darknery95@gmail.com>
2023-09-15 23:51:17 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+        public static void main(String[] args) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("parsentev@yandex.ru", "Petr Arsentev");
+            map.put("parsentev89@yandex.ru", "Petr Arsentev Sergeevich");
+            map.put("petrkulikov@yandex.ru", "Petr Kulikov");
+            map.put("ingaivanova@yandex.ru", "Inga Ivanova");
+
+            for (String key : map.keySet()) {
+                String value = map.get(key);
+                System.out.println(key + " = " + value);
+            }
+        }
+    }

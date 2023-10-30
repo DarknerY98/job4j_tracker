ae858f85cef58c5e5d54d08143ede0e1300b27f9
DarknerY98 <darknery95@gmail.com>
2023-10-30 22:30:46 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,32 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    {
+         int sizeOfEmpty = 10;
+
+         String surname;
+
+         final String newValue = "";
+
+    }
+
+    void echo() {
+    }
+
+          void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+        void method(int a, int b, int c, int d, int e, int f, int g) {
+
+        }
+
+        String name;
+
+       void broken() {
+    }
+}
+
